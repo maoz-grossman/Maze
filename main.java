3ced80b93ead4227aa55aef0512ba7f599eff6f1
Maoz-Grossman <44970530+maoz-grossman@users.noreply.github.com>
2019-07-16 16:13:51 +0300

    change main

--- main.java
+++ main.java
@@ -2,29 +2,23 @@
 public class main {
 
 	public static void main(String[] args) {
-		char[][]maze= {{'#','S','#','#','#','#','#','#','#'},
-				{'#',' ','#',' ','#',' ',' ',' ','#'},
-				{'#',' ',' ',' ','#',' ','#',' ','#'},
-				{'#','#','#',' ','#',' ','#',' ','#'},
-				{'#',' ',' ',' ','#',' ','#',' ','#'},
-				{'#',' ',' ',' ',' ',' ','#',' ','#'},
-				{'#',' ',' ','#',' ','#',' ',' ','#'},
-				{'#','#','#','#','#','#','E','#','#'}
-		};
+		
+		char [][]maze= Maze.get_random_maze(21, 30,'S','E','■');
 		Maze.printMaze(maze);
-
-
-		int[][]mazename=Maze.Vertecies_name(maze);
-
-		Graph g= new Graph(mazename);
+		int[][]mazename=Maze.Vertecies_name(maze,'S','E','■');
+		Graph g= new Graph(mazename,1);
 
 		Vertex _s= Maze.get_start_point();
 		BFS bfs=new BFS(g, _s);
-		bfs.Get_path(Maze.get_end_point(),maze);
-		System.out.println("\n---solution---\n");
+		 bfs.Get_path(Maze.get_end_point(),maze);
+		System.out.println("\n\t\t---solution---\n");
 
 		Maze.printMaze(maze);
+
 	}
 
 
 }
+
+
+
